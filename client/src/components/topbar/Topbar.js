00f7771b96e3410008d30478b5246ab046239039
Sulaiman <skshanwari830@gmail.com>
2021-11-03 16:06:52 -0400

    client side initital setup with few components done.

---
+++ client/src/components/topbar/Topbar.js
@@ -0,0 +1,38 @@
+import "./topbar.css"
+import {Search, Person, Chat, Notifications} from "@mui/icons-material"
+const Topbar = () => {
+    return (
+        <div className="topbar-container">
+            <div className="topbar-left">
+                <span className="logo">Shinsocial</span>
+            </div>
+            <div className="topbar-center">
+                <div className="search-bar">
+                    <Search className="search-icon"/>
+                    <input placeholder="Search for friend, post or video" type="text" className="search-input" />
+                </div>
+            </div>
+            <div className="topbar-right">
+                <div className="topbar-links">
+                    <span className="topbar-link">Homepage</span>
+                    <span className="topbar-link">Timeline</span>
+                </div>
+                <div className="topbar-icons">
+                <div className="topbar-icon-item">
+                        <Person/>
+                        <span className="topbar-icon-badge">1</span>
+                    </div><div className="topbar-icon-item">
+                        <Chat/>
+                        <span className="topbar-icon-badge">2</span>
+                    </div><div className="topbar-icon-item">
+                        <Notifications/>
+                        <span className="topbar-icon-badge">1</span>
+                    </div>
+                </div>
+                <img src="/assets/person/1.jpeg" alt="user profile image" className="topbar-img" />
+            </div>
+        </div>
+    )
+}
+
+export default Topbar
