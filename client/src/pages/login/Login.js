66734b04a02e813fd1180407163eabc1e1b5debd
Sulaiman <skshanwari830@gmail.com>
2021-11-08 16:05:16 -0500

    Auth Context created.

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,6 +1,14 @@
+import {useRef} from 'react'
 import "./login.css"
 
 const Login = () => {
+    const email = useRef()
+    const password = useRef()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('submitted')
+    }
     return (
         <div className="login">
             <div className="login-wrapper">
@@ -11,13 +19,26 @@ const Login = () => {
                     </span>
                 </div>
                 <div className="login-right">
-                    <div className="login-box">
-                        <input placeholder="Email" type="email" className="login-input" />
-                        <input placeholder="Password" type="password" className="login-input" />
+                    <form className="login-box" onSubmit={handleSubmit}>
+                        <input 
+                            placeholder="Email" 
+                            type="email" 
+                            className="login-input" 
+                            ref={email}
+                            required
+                        />
+                        <input 
+                            placeholder="Password" 
+                            type="password" 
+                            className="login-input" 
+                            ref={password}
+                            required 
+                            minLength="6"
+                        />
                         <button className="login-btn">Log In</button>
                         <span className="login-forgot">Forgot Password?</span>
                         <button className="signup-btn">Create a new account</button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
