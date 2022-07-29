a42f5feff4c6f66599894af6104c79b2ea6ebe97
sulaiman <sulaimanshinwari830@gmail.com>
2022-07-30 00:01:17 +0500

    added new login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,32 @@
 import { useContext } from 'react';
-import Home from './pages/home/Home'
-import Profile from "./pages/profile/Profile"
-import Login from "./pages/login/Login"
-import Register from "./pages/register/Register";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import {AuthContext} from './context/AuthContext'
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthContext } from './context/AuthContext';
 import RequireAuth from './components/auth';
+import NewLogin from './pages/new/login';
 
 function App() {
-  const {user} = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={ 
-       <RequireAuth><Home /> </RequireAuth>
-      } />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/profile/:username" element={<Profile />}/>
-    </Routes>
-  </Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <Home />{' '}
+            </RequireAuth>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/new-login" element={<NewLogin />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
