e4e6b2bcb77e8368403fdf581581ef12a639143b
sulaiman830 <skshanwari830@gmail.com>
2022-09-28 14:16:00 +0500

    file upload initial work

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const multer = require("multer");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
@@ -34,6 +35,30 @@ db.once("open", function (err, resp) {
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+const upload = multer();
+
+app.post(
+  "api/upload",
+  upload.single("file"),
+  (req, res) => {
+    try {
+      return res
+        .status(200)
+        .json("file uploaded successfully!");
+    } catch (err) {
+      console.log("file upload fialed", err);
+    }
+  }
+);
 
 app.get("/", (req, res) => {
   res.send("Welcome to homepage!");
