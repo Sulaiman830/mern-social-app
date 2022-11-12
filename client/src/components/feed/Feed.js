89dd426f5d24138f1b6401afce88e90a399ccd92
sulaiman830 <skshanwari830@gmail.com>
2022-11-12 21:15:03 +0500

    file upload code changes in Feed component

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,42 +1,38 @@
-import {useState, useEffect, useContext} from "react"
-import axios from 'axios'
+import { useState, useEffect, useContext } from "react"
+import axios from "axios"
 import Post from "../post/Post"
 import Share from "../share/Share"
 import "./feed.css"
 import { AuthContext } from "../../context/AuthContext"
 // import { Posts } from "../../dummyData"
 
-const Feed = ({username}) => {
+const Feed = ({ username }) => {
+  const [posts, setPosts] = useState([])
+  const { user } = useContext(AuthContext)
 
-    const [posts, setPosts] = useState([])
-    const {user} = useContext(AuthContext)
+  console.log("user", user)
 
-    console.log('user', user)
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = username
+        ? await axios.get("/posts/profile/" + username)
+        : await axios.get("/posts/timeline/" + user._id)
+      setPosts(res.data)
+    }
+    fetchPosts()
+  }, [username, user._id])
 
-    useEffect(() => {
-       const fetchPosts = async () => {
-        const res = username ? 
-            await axios.get("/posts/profile/" + username) : 
-            await axios.get("/posts/timeline/" + user._id)
-        setPosts(res.data)
-       }
-       fetchPosts();
-    }, [username, user._id])
-
-    return (
-        <div className="feed">
-            <div className="feed-wrapper">
-                <Share/>
-                {posts.map((post) => {
-                    console.log('post', post)
-                    return (
-                        <Post key={post._id} post={post}/>
-                    )  
-                })}
-                
-            </div>
-        </div>
-    )
+  return (
+    <div className="feed">
+      <div className="feed-wrapper">
+        <Share />
+        {posts.map((post) => {
+          console.log("post", post)
+          return <Post key={post._id} post={post} />
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Feed
